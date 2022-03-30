2fe4a152d8e0ef5ff343d862ffdae1600b2ce925
Chaos <antonioscutari2@hotmail.com>
2022-03-30 20:27:04 -0300

    Forgot sumthin

--- client/src/components/Creator.jsx
+++ client/src/components/Creator.jsx
@@ -1,6 +1,32 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 const Creator = () => {
+  let dispatch = useDispatch();
+  let [error, setError] = useState({});
+  let [input, setInput] = useState({
+    id: "",
+    image: "",
+    title: "",
+    summary: "",
+    score: "",
+    h_score: "",
+    steps: "",
+    diets: "",
+    price: "",
+  });
+  let validate = () => {
+    let error = {};
+    if (!input.title) {
+      error.title = "Title required";
+    } else if (!input.summary) {
+      error.summary = "Sum summary pls";
+    } else if (!input.steps) {
+      error.steps = "Tell us how";
+    } else if (!input.diets) {
+      error.steps = "Define a diet type";
+    }
+    return error;
+  };
   return <div>Creator</div>;
 };
 
